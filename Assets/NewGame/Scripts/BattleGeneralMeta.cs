71be8f02c350e4862c8ada5c7321a4ede5d4ef81
Mauve3 <mauvemoonman@gmail.com>
2016-07-17 09:37:39 -0700

    added toolbar

---
+++ Assets/NewGame/Scripts/BattleGeneralMeta.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BattleGeneralMeta : MonoBehaviour {
+
+	public int tactics = 2;
+	public List<GameObject> army;
+
+	public BattleGeneralMeta(BattleGeneralMeta general){
+		this.tactics = general.tactics;
+		this.army = general.army;
+	}
+
+	void OnDisable(){
+		Debug.Log ("OnDisable");
+	}
+
+	void OnDestroy(){
+		Debug.Log ("OnDestroy");
+	}
+}
---
+++ Assets/NewGame/Scripts/UI/Panel.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class Panel : MonoBehaviour {
+
+	private Transform selectedChild;
+	private List<Transform> children;
+	private BattleLoader game;
+
+	void Start(){
+
+		children = new List<Transform> ();
+		foreach (Transform child in gameObject.transform) {
+			children.Add (child);
+		}
+		selectedChild = children[0];
+	}
+
+	Transform myRayHit(Vector2 click){
+		Debug.Log ("Looking: " + click.ToString());
+		foreach (Transform child in gameObject.transform) {
+			Rect rt = ((RectTransform) child).rect;
+
+			Rect rect = new Rect (child.position.x - (rt.width/2), child.position.y - (rt.height/2), rt.width, rt.height);
+
+			Debug.Log ("Found: " + child.transform + " pos: " + rect.position.ToString() + " dims: " + rect.width + ":" + rect.height);
+
+			if (rect.Contains (Input.mousePosition)) {
+				return child;
+			}
+		} 
+		return null;
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown (0)) {
+			Transform child = myRayHit (Input.mousePosition);
+			if (child != null) {
+				selectedChild = child;
+
+				Debug.Log ("Name: " + GameObject.Find ("BattleCamera").name);
+				//Debug.Log ("Name: " + GameObject.Find ("Main Camera").name);
+
+				game = GameObject.Find ("BattleCamera").GetComponent<BattleLoader>();
+				game.getGameManager ().panelClicked (child.gameObject);
+			}
+		}
+	}  
+
+	public Transform getSelected(){
+		return selectedChild;
+	}
+
+	public void removeSelected(){
+		children.Remove(selectedChild);
+		selectedChild = children[0];
+	}
+}
