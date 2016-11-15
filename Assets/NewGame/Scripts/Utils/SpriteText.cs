52303800627eb6f4b6aea660e1abcf053f4f985c
Mauve3 <mauvemoonman@gmail.com>
2016-11-15 00:18:35 -0800

    updates

---
+++ Assets/NewGame/Scripts/Utils/SpriteText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteText : MonoBehaviour {
+
+	void refresh(){
+		var parent = transform.parent;
+
+		var parentRenderer = parent.GetComponent<Renderer>();
+		var parentMeta = parent.GetComponent<BattleMeta>();
+		var renderer = GetComponent<Renderer>();
+		renderer.sortingLayerID = parentRenderer.sortingLayerID;
+		renderer.sortingOrder = parentRenderer.sortingOrder;
+
+		var spriteTransform = parent.transform;
+		var text = GetComponent<TextMesh>();
+		var pos = spriteTransform.position;
+		text.text = string.Format("{0}", parentMeta.getLives());
+	}
+
+	// Use this for initialization
+	//void Start () {
+	//	refresh ();
+	//}
+
+	void Update () {
+		refresh ();
+	}
+}
