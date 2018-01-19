0b7b684ff1ad77938aa00a6e3f75d25e2c2cd2b3
Mauve3 <mauvemoonman@gmail.com>
2018-01-18 21:30:12 -0800

    for save

---
+++ Assets/NewGame/Scripts/Battle/BattleLevels.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleLevels : MonoBehaviour {
+	public string name;
+	public enum lvltype 
+	{
+		Cave, Dungeon, Magical, Palace, World
+	};
+	public lvltype lvl;
+	public GameObject[] outerWallTiles;
+	public GameObject[] innerWallTiles;
+	public GameObject[] floorTiles;
+}
