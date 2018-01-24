6f9ed85c311db47a51b75a1d1977d5be3316689d
Mauve3 <mauvemoonman@gmail.com>
2018-01-23 22:45:28 -0800

    for save

---
+++ Assets/NewGame/Scripts/Battle/BattleAttributes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleAttributes : MonoBehaviour {
+	public string name;
+	public string description;
+
+	public bool atk_all = false;
+}
