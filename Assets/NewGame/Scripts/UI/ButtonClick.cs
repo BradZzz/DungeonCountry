b1a5c63f41b6745fdd180ee8b54203e226de1c1c
Mauve3 <mauvemoonman@gmail.com>
2018-01-27 17:55:08 -0800

    for save

---
+++ Assets/NewGame/Scripts/UI/ButtonClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class ButtonClick : MonoBehaviour {
+	public void click() {
+		Application.LoadLevel ("PuzzleScene");
+	}
+}
